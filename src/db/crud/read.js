ae4451c94f590fe682753afa9cde88ea3a5d6811
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-05 19:31:12 -0300

    Agrego cambios para el uso de PostgreSQL

--- src/db/crud/read.js
+++ src/db/crud/read.js
@@ -2,10 +2,8 @@ let db = require('../dbConnection')
 
 function read_project(){
     return new Promise( (resolve,reject) => {
-        db.connect()
         db.query("SELECT id, name, description, TO_CHAR(start, 'dd/mm/yyyy') as start, TO_CHAR(finish, 'dd/mm/yyyy') as finish, state, leader, team FROM projects LEFT JOIN (SELECT id_project, array_agg(id_employee) as team FROM project_teams group by id_project) as teams ON projects.id = teams.id_project;", (err, res) => {
             if (err) reject(err);
-            db.end()
             resolve(res.rows);    
         })
     });   
@@ -13,10 +11,8 @@ function read_project(){
 
 function read_task(project){
     return new Promise( (resolve,reject) => {
-        db.connect()
         db.query("SELECT id, name, description, state, team FROM tasks LEFT JOIN (SELECT id_task, array_agg(id_employee) as team FROM task_teams GROUP BY id_task) as teams ON tasks.id = teams.id_task WHERE id = $1;",[project.get_id()], (err, res) => {
             if (err) reject(err);
-            db.end()
             resolve(res.rows);    
         })
     });   
