ad2224dd3db1cd6cecc43bd370d79c76c22ac669
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-05 16:54:41 -0300

    Anulo db para probar heroku

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+/*var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
@@ -10,4 +10,4 @@ var connection = mysql.createConnection({
  
 connection.connect();
 
-module.exports = connection;
+module.exports = connection;*/
