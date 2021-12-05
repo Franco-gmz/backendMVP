f6746f00829fea9aa6f662eca116cf1abdc79196
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-05 17:18:24 -0300

    Heroku fix

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,3 +1,20 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('CREATE TABLE [IF NOT EXISTS] projects (id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,start DATE NOT NULL,finish DATE NOT NULL,name VARCHAR(20) NOT NULL,description VARCHAR(50) NOT NULL,leader INT UNSIGNED,state VARCHAR(15) NOT NULL);', (err, res) => {
+  if (err) console.log(err.code);
+  else console.log("Projects created");
+})
+
+module.exports = connection;
 /*var mysql = require('mysql');
 
 var connection = mysql.createConnection({
