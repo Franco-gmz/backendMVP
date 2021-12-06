56ba6347f4c9c928dc80fe0826a02a3c9c1c4262
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-06 15:46:41 -0300

    Uso params en GET task

--- src/routes/router.js
+++ src/routes/router.js
@@ -47,7 +47,7 @@ router.post('/task', function(req,res){
           .catch( (err) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
 });
 
-router.get('/task/:id_project', function(req,res){  
+router.get('/task/:id', function(req,res){  
     let project = new Project(req.params)
     read.task(project)
         .then( (results) => res.status(200).send({results: results}))
