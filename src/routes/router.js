196f783030b580485fbb2f2898ee9fc35002553c
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-08 12:22:00 -0300

    modify cors

--- src/routes/router.js
+++ src/routes/router.js
@@ -18,6 +18,8 @@ router.get('/project', function(req,res){
 
 router.post('/project', function(req,res){
     let project = new Project(req.body)
+    console.log("BODY:\n",req.body);
+    console.log("project:\n",project);
     create.project(project)
           .then( (result) => res.status(200).send({results: result}))
           .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
@@ -25,8 +27,6 @@ router.post('/project', function(req,res){
 
 router.put('/project', function(req,res){
     let project = new Project(req.body)
-    console.log("BODY:\n",req.body);
-    console.log("project:\n",project);
     update.project(project, req.body.fields, req.body.values)
           .then( () => res.status(200).send({results: "OK"}))
           .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
