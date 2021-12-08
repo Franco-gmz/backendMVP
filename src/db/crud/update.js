f4e393c992cfc4f17ac5748706346cb71e4163f6
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-08 16:42:04 -0300

    modify update

--- src/db/crud/update.js
+++ src/db/crud/update.js
@@ -2,8 +2,6 @@ let db = require('../dbConnection')
 
 function create_query(table, fields, values){
     let querySQL = 'UPDATE ' + table + ' SET ';
-    console.log("FIELD:\n",fields)
-    console.log("VALUES:\n",values)
     fields.forEach( function(field,i){
         querySQL = querySQL + field + ' = ' + "'" + values[i] + "' ";
         if (i < fields.length - 1) querySQL = querySQL + ', ';
