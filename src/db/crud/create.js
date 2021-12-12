7653fc388d552ecd7a308769aa0f62a41ddc132c
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-12 19:51:26 -0300

    Refactor

--- src/db/crud/create.js
+++ src/db/crud/create.js
@@ -2,7 +2,7 @@ let db = require('../dbConnection')
 
 function query_checking_leader(project){
     if (project.leader == undefined) {
-        return 'INSERT INTO projects (start, finish, name, description, state) VALUES ($1, $2, $3, $4, $5);';
+        return 'INSERT INTO projects (start, finish, name, description, state) VALUES ($1, $2, $3, $4, $5) RETURNING (start, finish, name, description, state,leader);';
     }
     return 'INSERT INTO projects (start, finish, name, description, leader, state) VALUES ($1, $2, $3, $4, $5, $6);';
 }
@@ -15,7 +15,6 @@ function values_checking_leader(project){
 }
 
 function create_project(project){
-    console.log("PROYECTO:\n",project);
     return new Promise( (resolve, reject) => {
         db.query(query_checking_leader(project),values_checking_leader(project), (err, res) => {
             if (err){
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,122 +1,127 @@
-var Project = require('../db/entity/project.js')
-var Task = require('../db/entity/task.js')
 var router = require('express').Router();
 
-//CRUD
+// Entities
+var Project = require('../db/entity/project.js')
+var Task = require('../db/entity/task.js')
+
+
+// CRUD
 let create = require('../db/crud/create.js')
 let read = require('../db/crud/read.js')
 let erase = require('../db/crud/delete.js')
 let update = require('../db/crud/update.js')
 
-//PROJECTS
+// Projects endpoints
 
+// get all projects and their teams
 router.get('/project', function(req,res){  
     read.project()
         .then( (results) => res.status(200).send({results: results}))
-        .catch( (errcode) => res.status(500).send({results:errcode})) //Actuar segun #errcode
+        .catch( (errcode) => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
+// create a project
 router.post('/project', function(req,res){
     let project = new Project(req.body)
     create.project(project)
-          .then( (result) => res.status(200).send({results: result}))
-          .catch( (errcode) => {
-              res.status(500).send({results:"Error"})
-            }) //Actuar segun #errcode
+          .then( () => res.status(200).send({results: project}))
+          .catch( errcode => res.status(500).send({results:"Ha ocurrido un error"}))
+
 });
 
+// update project
 router.put('/project', function(req,res){
     let project = new Project(req.body)
-    console.log("BODY:\n",req.body);
     update.project(project, req.body.fields, req.body.values)
-          .then( () => res.status(200).send({results: "OK"}))
-          .catch( (errcode) => {
-              console.log("ERROR: ",errcode)
-              res.status(500).send({results:"Error"})}) //Actuar segun #errcode
-        
+          .then( () => res.status(200).send({results: project }))
+          .catch( errcode => res.status(500).send({results:"Ha ocurrido un error"}))     
 });
 
+// delete project
 router.delete('/project/:id', function(req,res){
     let project = new Project(req.params)
     erase.project(project)
-         .then( () => res.status(200).send({results: "OK"}))
-         .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
+         .then( () => res.status(200).send({results: "El proyecto ha sido eliminado correctamente"}))
+         .catch( errcode => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
-//TASKS
+// Task endpoints
 
+// create task
 router.post('/task', function(req,res){
     let task = new Task(req.body)
     create.task(task)
-          .then( (result) => res.status(200).send({results: result}))
-          .catch( (err) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
+          .then( (task_id) => res.status(200).send({id: task_id}))
+          .catch( (err) => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
+// get task by its id
 router.get('/task/:id', function(req,res){  
     let task = new Task(req.params)
-    console.log(task)
     read.task(task)
         .then( (results) => res.status(200).send({results: results}))
-        .catch( (errcode) => {
-            console.log(errcode)
-            res.status(500).send({results:"Error"})}) //Actuar segun #errcode
+        .catch( errcode => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
+// get all tasks in a project by its id
 router.get('/task/set/:id', function(req,res){  
     let project = new Project(req.params)
     read.taskSet(project)
         .then( (results) => res.status(200).send({results: results}))
-        .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
+        .catch( errcode => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
+// update a task
 router.put('/task', function(req,res){
     let task = new Task(req.body)
     update.task(task, req.body.fields, req.body.values)
-          .then( () => res.status(200).send({results: "OK"}))
-          .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
+          .then( () => res.status(200).send({results: task}))
+          .catch( (errcode) => res.status(500).send({results:"Ha ocurrido un error"}))
         
 });
 
+// delete a task
 router.delete('/task/:id', function(req,res){
     let task = new Task(req.params)
     erase.task(task)
-         .then( () => res.status(200).send({results: "OK"}))
-         .catch( errcode => {
-             console.log(errcode);
-             res.status(500).send({results:"Error"});
-          }) //Actuar segun #errcode
+         .then( () => res.status(200).send({results: "La tarea ha sido eliminada correctamente"}))
+         .catch( errcode => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
-// PROJECT TEAMS
+// Team project
 
+// update team  of a project
 router.put('/project/team', function(req,res){
     let project = new Project(req.body)
     update.project_team(project)
           .then( (result) => res.status(200).send({results: result}))
-          .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
+          .catch( errcode => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
+// delete team members of a project
 router.delete('/project/team', function(req,res){
     let project = new Project(req.body)
     erase.project_team(project)
-         .then( () => res.status(200).send({results: "OK"}))
-         .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
+         .then( () => res.status(200).send({results: "Se ha eliminado correctamente"}))
+         .catch( (errcode) => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
-// TASK TEAM
+// Team task
 
+// update team members of a task
 router.put('/task/team', function(req,res){
     let task = new Task(req.body)
     update.task_team(task)
           .then( (result) => res.status(200).send({results: result}))
-          .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
+          .catch( (errcode) => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
+// delete team members of a task
 router.delete('/task/team', function(req,res){
     let task = new Task(req.body)
     erase.task_team(task)
          .then( () => res.status(200).send({results: "OK"}))
-         .catch( (errcode) => res.status(500).send({results:"Error"})) //Actuar segun #errcode
+         .catch( (errcode) => res.status(500).send({results:"Ha ocurrido un error"}))
 });
 
 module.exports = router;
