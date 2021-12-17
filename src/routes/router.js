e20a3a9829e453a9299299f72abf87e027cda264
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-17 20:15:28 -0300

    fix delete team task

--- src/routes/router.js
+++ src/routes/router.js
@@ -142,9 +142,13 @@ router.put('/team/task', function(req,res){
 // delete team members of a task
 router.delete('/team/task/:id/:team', function(req,res){
     let task = new Task(req.params)
+    console.log(task)
     erase.task_team(task)
          .then( () => res.status(200).send({results: "OK"}))
-         .catch( (errcode) => res.status(500).send({results:"Ha ocurrido un error"}))
+         .catch( (errcode) => {
+             console.log(errcode)
+             res.status(500).send({results:"Ha ocurrido un error"})
+        })
 });
 
 module.exports = router;
