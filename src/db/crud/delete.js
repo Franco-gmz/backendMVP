445a1615db2f927fcf5386b4b3e03b6c9d2b2cd8
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-18 12:39:40 -0300

    fix delete project

--- src/db/crud/delete.js
+++ src/db/crud/delete.js
@@ -6,7 +6,7 @@ function erase_project(project){
         db.query("DELETE FROM projects WHERE id = $1;",[project.get_id()], (err, res) => {
             if (err) reject(err);    
         })
-        db.query("SELECT id_project FROM task WHERE id_project = $1;",[project.get_id()], (err, res) => {
+        db.query("SELECT id FROM tasks WHERE id_project = $1;",[project.get_id()], (err, res) => {
             if (err) reject(err);
             if(res){
                 db.query("DELETE FROM task_teams WHERE id_task = ANY($1);",res.rows, (err, res) => {
@@ -14,12 +14,8 @@ function erase_project(project){
                 })
             }   
         })
-        db.query("DELETE FROM task WHERE id_project = $1;",[project.get_id()], (err, res) => {
+        db.query("DELETE FROM tasks WHERE id_project = $1;",[project.get_id()], (err, res) => {
             if (err) reject(err);    
-        })
-        
-        db.query("DELETE FROM project_teams WHERE id_project = $1;",[project.get_id()], (err, res) => {
-            if (err) reject(err);   
         })
         resolve();
     });   
