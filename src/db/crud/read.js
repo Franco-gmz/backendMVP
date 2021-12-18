42332109e6a3226848c30423edd1515d577321ef
Franco Gomez <fdgomez@fi.uba.ar>
2021-12-18 11:35:28 -0300

    push

--- src/db/crud/read.js
+++ src/db/crud/read.js
@@ -4,7 +4,8 @@ function read_project(project){
     return new Promise( (resolve,reject) => {
         db.query("SELECT id, name, description, TO_CHAR(start, 'dd/mm/yyyy') as start, TO_CHAR(finish, 'dd/mm/yyyy') as finish, state, leader, team FROM projects LEFT JOIN (SELECT id_project, array_agg(id_employee) as team FROM project_teams group by id_project) as teams ON projects.id = teams.id_project WHERE id = $1;",[project.get_id()] ,(err, res) => {
             if (err) reject(err);
-            resolve(res.rows);    
+            if(res != undefined) resolve(res.rows);
+            else resolve();    
         })
     });   
 }
@@ -14,7 +15,8 @@ function read_projects(){
     return new Promise( (resolve,reject) => {
         db.query("SELECT id, name, description, TO_CHAR(start, 'dd/mm/yyyy') as start, TO_CHAR(finish, 'dd/mm/yyyy') as finish, state, leader, team FROM projects LEFT JOIN (SELECT id_project, array_agg(id_employee) as team FROM project_teams group by id_project) as teams ON projects.id = teams.id_project;", (err, res) => {
             if (err) reject(err);
-            resolve(res.rows);    
+            if(res != undefined) resolve(res.rows);
+            else resolve();    
         })
     });   
 }
@@ -24,7 +26,8 @@ function read_task_set(project){
     return new Promise( (resolve,reject) => {
         db.query("SELECT id, name, description, state, team FROM tasks LEFT JOIN (SELECT id_task, array_agg(id_employee) as team FROM task_teams GROUP BY id_task) as teams ON tasks.id = teams.id_task WHERE id_project = $1;",[project.get_id()], (err, res) => {
             if (err) reject(err);
-            resolve(res.rows);    
+            if(res != undefined) resolve(res.rows);
+            else resolve();    
         })
     });   
 }
@@ -34,7 +37,8 @@ function read_task(task){
     return new Promise( (resolve,reject) => {
         db.query("SELECT id, name, description, state, id_project FROM tasks WHERE id = $1;",[task.get_id()], (err, res) => {
             if (err) reject(err);
-            resolve(res.rows);    
+            if(res != undefined) resolve(res.rows);
+            else resolve();    
         })
     });   
 }
@@ -44,7 +48,8 @@ function read_all_tasks(){
     return new Promise( (resolve,reject) => {
         db.query("SELECT * FROM tasks;", (err, res) => {
             if (err) reject(err);
-            resolve(res.rows);    
+            if(res != undefined) resolve(res.rows);
+            else resolve();    
         })
     }); 
 }
